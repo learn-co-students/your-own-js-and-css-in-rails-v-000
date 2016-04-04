70783bf0f36b68d3e046c3ca81fe5ec5c53304c8
Marco Tsui <marcotsui2003@yahoo.com>
2016-04-04 17:50:03 -0400

    done

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,15 @@
+$('#hide_this').click(function(event) {
+  //event.preventDefault;
+  $('#hide_this').hide();
+})
+
+
+
+//The correct way to do this is to use show and hide:
+
+//$('#id').hide();
+//$('#id').show();
+//An alternate way is to use the jQuery css method:
+
+//$("#id").css("display", "none");
+//$("#id").css("display", "block");
