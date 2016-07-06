0d96ea037127a47ed0230eaa8fe5d9da12eae079
McN33ly <rmcneely23@gmail.com>
2016-07-05 23:37:05 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  require 'selenium/webdriver'
+
+  Selenium::WebDriver::Firefox::Binary.path =
+    "/usr/lib/firefox/firefox"
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
