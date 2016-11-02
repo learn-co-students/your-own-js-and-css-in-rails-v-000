379fc9766f1f89e7c65c0760897d11cc00dd0926
Christian Whitesides <whitesidescr@gmail.com>
2016-11-02 12:27:45 -0700

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -7,3 +7,14 @@ function hideWhenClicked() {
     $(this).hide();
   });
 }
+
+// The solution uses the event object (event) received by the event handling
+// function (hideWhenClicked()) as part of the solution.
+// See: https://learn.jquery.com/events/event-basics/#inside-the-event-handler-function
+
+// function hideWhenClicked(event) {
+//   var $target = $(event.target);
+//   $target.hide();
+// }
+//
+// $('#hide_this').click(hideWhenClicked);
