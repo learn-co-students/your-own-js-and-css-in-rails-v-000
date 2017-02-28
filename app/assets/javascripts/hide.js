f6b6d0cd9888482ae1b2623a512e073304314f3c
Adam Lucia <lucia.adam@gmail.com>
2017-02-28 22:35:57 +0000

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,7 @@
+document.getElementById('hide_this').addEventListener("click", hideWhenClicked);
+
+function hideWhenClicked() {
+//  document.getElementById("hide_this").text = "";
+  $("#hide_this").hide();
+  debugger
+}
