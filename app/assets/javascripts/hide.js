64dbd654380716e1f477b2e9c2c07fb8950013ab
Ivan Pilot <ivanpilot@gmail.com>
2017-04-28 20:38:00 +0200

    new try

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-
-  console.log('I am ready')
-
-  // var el = $("#hide_this")[0]
-  // // debugger
-  hideWhenClicked.bind(this)
-
-})
+// $(document).ready(function(){
+//
+//   console.log('I am ready')
+//
+//   // var el = $("#hide_this")[0]
+//   // // debugger
+//   hideWhenClicked.bind(this)
+//
+// })
 
 
 function hideWhenClicked(){
