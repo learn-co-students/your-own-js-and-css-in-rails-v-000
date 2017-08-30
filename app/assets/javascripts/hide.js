42feeaeaa4a5cab303333c3c5ae1816b53f3e7a5
lawrend <douglas_lawrence@mac.com>
2017-08-30 01:40:51 -0700

    done

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -2,3 +2,10 @@ function hideWhenClicked() {
     var x = document.getElementById("hide_this");
     x.style.display = 'none'
 }
+
+// function hideWhenClicked(event) {
+//     var $target = $(event.target);
+//     $target.hide();
+// }
+
+// $('#hide_this').click(hideWhenClicked);
