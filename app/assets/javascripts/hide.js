c188cd92db81246e347dbbc870ee55f2eaceee49
github username <github email address>
2018-01-01 16:59:57 -0500

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,6 @@
+const el = document.getElementById('hide_this')
+el.addEventListener('click', function() { this.style.display = 'none' })
+
+// function hideWhenClicked() {
+//   el.style.display = 'none'
+// }
