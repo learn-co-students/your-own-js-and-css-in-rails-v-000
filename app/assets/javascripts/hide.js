3ee50e8d0309b86c9c2a3dc7c967c8343ae28b40
github username <juliannne@gmail.com>
2018-01-17 17:59:19 -0500

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,6 @@
-function hideWhenClicked() {
-  $('#hide_this').hide()
+function hideWhenClicked(event) {
+  var target = $(event.target);
+  target.hide();
 }
+
+$('#hide_this').click(hideWhenClicked);
