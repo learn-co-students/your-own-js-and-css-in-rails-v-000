eba0f67f01392c4c166293e4899f937b5b94b464
badlychadly <chadmontoya21@yahoo.com>
2018-07-03 16:33:44 -0400

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,8 @@
 
 
-function hideWhenClicked() {
-  $("#hide_this").hide()
+function hideWhenClicked(event) {
+  var $target = $(event.target);
+  $target.hide();
 }
+
+$('#hide_this').click(hideWhenClicked);
