36aef6410adb016725fd8bdc1a57e0434b76b74f
nmlstyl <joey.michalina.mariano@gmail.com>
2018-07-16 23:28:30 -0400

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,5 @@
-function hideWhenClicked(){
-  $(`#hide_this`).css("display", "none")
-}
 
-$(document).click("#hide_this");
+
+$('#hide_this').bind("click", function hideWhenClicked(){
+  $(`#hide_this`).hide()
+})
