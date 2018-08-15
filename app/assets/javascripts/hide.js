d30fb5a6d0ce7ebcaf61710b37c09a16cfc76ca9
Dee Race <deerace12@gmail.com>
2018-08-15 19:01:25 +0000

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,11 @@
 function hideWhenClicked() {
   document.getElementById('hide_this').style.visibility = "hidden";
 }
+
+
+// function hideWhenClicked(event) {
+//    var $target = $(event.target);
+//    $target.hide();
+// }
+
+// $('#hide_this').click(hideWhenClicked);
