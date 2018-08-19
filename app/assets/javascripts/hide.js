6fd5cc6faae49391e1c07eebdeef05e9d8c5ec98
Maverick Tesini <mavericktesini@gmail.com>
2018-08-19 06:55:10 +0000

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,8 @@
+// In /app/assets/javascripts/ create the file hide.js
+// Create the function hideWhenClicked that hides the element that was clicked.
+// Make sure to bind to #hide_this
+// Add hide.js to your JS manifest.
+
+function hideWhenClicked() {
+  document.getElementById('#hide_this');
+}
