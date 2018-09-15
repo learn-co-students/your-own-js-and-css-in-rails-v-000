035d7b21b14ec01b1ac8cfe829a1e27f3c26d064
barbara.prusiewicz prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-15 00:12:14 +0000

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,7 @@
 function hideWhenClicked() {
+  console.log("hiding!");
   document.getElementById("hide_this").style.display = "none";
 }
+
+// instead of adding hideWhenClicked() call in html, can do the following here
+//$('#hide_this').click(hideWhenClicked);
