20d7f44ee8b1df614eebd714aee9f08763af4e2b
Jamie Brown <jmbrowncow@gmail.com>
2018-09-28 21:47:46 +0000

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,11 @@
+
+  $("#hide_this").on('click', function(){
+      $("#hide_this").hide();
+  })
+
+//
+// const hide = document.getElementById("hide_this");
+//
+// hide.addEventLIstenr('click', function(){
+//   this.hide;
+// })
