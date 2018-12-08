ad9759f72ed23574c58ed42ad8686e8d692f3a14
Carol Greene <carolgreene3@gmail.com>
2018-12-08 21:58:33 +0000

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,11 @@
 function hideWhenClicked() {
-  document.getElementById("hide_this").style.visibility = "hidden";
+  document.getElementById("hide_this").style.visibility = "hidden";  //can also use .style.display = "none"
 }
+
+//learn's solution:
+//function hideWhenClicked(event) {
+//var $target = $(event.target);
+//$target.hide();
+//}
+
+//$('hide_this').click(hideWhenClicked);      ** can call the function here instead of putting hideWhenClicked() in link
