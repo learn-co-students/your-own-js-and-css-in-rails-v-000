e2fcc84592a789dc442d40fd57a52957373ab965
github username <github email address>
2018-12-21 15:10:13 -0500

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+# require 'capybara/poltergeist'
+# Capybara.javascript_driver = :poltergeist
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -48,4 +48,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
