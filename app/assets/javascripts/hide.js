0bab530380d74d632319f502ec103169c4b1e691
Madeline-Stark <madeline.stark1@gmail.com>
2019-01-29 13:17:58 -0800

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,29 @@
+// function hideWhenClicked() {
+//   const hidden = document.getElementByID('hide_this');
+//   hidden.addEventListener('click', function(event) {
+//     // hidden.style.display = 'none';
+//     // hidden.visibility = 'hidden';
+//      // hidden.hidden;
+//   })
+// }
+
+// const link = document.getElementByID('hide_this');
+// link.onclick = hideWhenClicked();
+
+// $('#hide_this').click(function(event) {
+//   $event.target.hide;
+// })
+
+// function hideWhenClicked(event) {
+//   $event.target.hide();
+// }
+//
+// $('#hide_this').click(hideWhenClicked);
+
+// const hideWhenClicked = $('#hide_this').click(function(event) {
+//   $event.target.hide();
+// })
+
+const hideWhenClicked = $('#hide_this').click(function() {
+  $('#hide_this').hide();
+})
