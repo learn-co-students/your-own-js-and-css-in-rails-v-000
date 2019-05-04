7a81e41051c53891ac1b499f57e2b59f4cfb2fc8
George  Pianka <gpianka1@gmail.com>
2019-05-04 16:22:31 +0000

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,14 @@
+/* function hideWhenClicked(event) {
+    var $target = $(event.target);
+    $target.hide();
+}
+
+ $('#hide_this').click(hideWhenClicked); 
+ 
+ */
+ 
+ 
+ 
+ //$('#hide_this').click( e => $(e.target).hide() );
+ 
+ $('#hide_this').click( function(e){ $(e.target).hide() } );
