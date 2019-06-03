844e5bfaf9ef3204ce2189e17e01d2dfff67f4a8
Erin Green <erin.y.green@gmail.com>
2019-06-03 19:50:08 +0000

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,8 @@
-function hideWhenClicked(el) {
-  el.addEventListener(click, function{
-    this.style.display = "none";
-  })
+
+
+function hideWhenClicked(event) {
+  var $target = $(event.target);
+  $target.hide();
 }
+
+$('#hide_this').click(hideWhenClicked);
