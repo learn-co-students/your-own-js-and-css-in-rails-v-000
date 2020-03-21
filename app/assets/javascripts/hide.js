e49a6e40d41336cc91ba0cb12124cbae2df3578d
RonSala <ron.sala@gmail.com>
2020-03-21 18:58:39 -0400

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,5 @@
 function hideWhenClicked() {
-  el = document.getElementById("hide_this");
+  var el = document.getElementById("hide_this");
   if (el.style.display === "none") {
     el.style.display = "block";
   } else {
